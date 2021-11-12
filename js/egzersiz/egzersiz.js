3c98154e2d06c224027b109bbddb9b01321324e9
Toygar Aksoy <toygar.aksoy@gmail.com>
2021-11-12 22:23:57 +0300

    odev3 index.html olusturuldu

--- js/egzersiz/egzersiz.js
+++ js/egzersiz/egzersiz.js
@@ -17,6 +17,7 @@ FF  0-49
 4- ff class bilgisi text-danger, digerlerinin ise text-primary olsun
 */
 
+/*
 const SMILE=`
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-smile" viewBox="0 0 16 16">
   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
@@ -60,3 +61,94 @@ if (grade>=0 && grade<=100) {
 let info = document.querySelector("#info");
 textInfo == "FF" ? info.classList.add("text-danger") : info.classList.add("text-primary");
 info.innerHTML = `${textInfo == "FF" ? textInfo = FROWN + textInfo : textInfo=SMILE+textInfo} => ${grade}`
+*/
+
+
+/*
+// ROMAN NUMERAL REDUCTION
+function RomanNumeralReduction(str) { 
+
+  // code goes here  
+  let m = new Map([
+    ["M",1000],
+    ["CM",900],
+    ["D",500],
+    ["CD",400],
+    ["C",100],
+    ["XC",90],
+    ["L",50],
+    ["XL",40],
+    ["X",10],
+    ["IX",9],
+    ["V",5],
+    ["IV",4],
+    ["I",1],
+    [1,"I"],
+    [4,"IV"],
+    [5,"V"],
+    [9,"IX"],
+    [10,"X"],
+    [40,"XL"],
+    [50,"L"],
+    [90,"XC"],
+    [100,"C"],
+    [400,"CD"],
+    [500,"D"],
+    [900,"CM"],
+    [1000,"M"]
+    ]);
+
+  let num = 0;
+  [...str].forEach(c => num+=m.get(c));
+
+  let res = "";
+  while (num>0) {
+    let n = [...m.values()].find(e => e <= num)
+    res += m.get(n)
+    num -= n
+  }
+
+  return res; 
+
+}
+   
+// keep this function call here 
+console.log(RomanNumeralReduction("IIIVVXXXCDD"));
+*/
+
+/*
+// iç içe elementler çağrıldı  
+const selectBox = document.querySelector('#selectColor');
+const button = document.querySelector('#button');
+const form = document.querySelector('.inputText');  
+const cerceve = document.querySelector('.container');
+selectBox.addEventListener('click' ,function(){
+    console.log("selectBox");    
+    })
+form.addEventListener('click',function(){
+    console.log("form");
+    })
+cerceve.addEventListener('click',function(){
+    console.log('cerceve');
+    })   
+*/
+
+/*
+let dizi = [2,5,8,11,15,17];
+
+let x = dizi.filter(n => n>10);
+let bes_x = x.map(sayi => sayi*5);
+
+console.log(bes_x)
+*/
+
+/*
+let dizi = [3,6,9,14,16];
+console.log(dizi.some(sayi => sayi>5) ? "Beşten büyük bir eleman mevcut" : "%'ten büyük eleman mevcut değil.")
+*/
+
+/*
+let dizi = [2,3,4];
+console.log(dizi.reduce((a,b)=>a*b,1))
+*/
+
